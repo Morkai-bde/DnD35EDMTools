9c3bb5b9e874b6ff09707842d21248b6b9283265
MGNThalentyr <morkai.bde@gmail.com>
2024-02-20 17:18:05 +0200

    Helpers and Tests
    Added more Conversion helpers and tests

--- DnD35EDMTools/Helpers/ConversionHelper.cs
+++ DnD35EDMTools/Helpers/ConversionHelper.cs
@@ -2,10 +2,44 @@
 
 public static class ConversionHelper
 {
-        public static string ConvertInchesToFeet(decimal heightInInches)
-        {
-            int feet = (int)(heightInInches / 12);
-            int inches = (int)(heightInInches % 12);
-            return $"{feet}'{inches}\"";
-        }
+    public static int ConvertFeetAndInchesToInches(string feetAndInches)
+    {
+        // Example input format: "6'1""
+        var parts = feetAndInches.Split('\'', '"');
+        if (parts.Length != 3)
+            throw new ArgumentException("Invalid format. Expected format: feet'inches\"");
+
+        int feet = int.Parse(parts[0]);
+        int inches = int.Parse(parts[1]);
+        return feet * 12 + inches;
+    }
+
+    public static string ConvertInchesToFeetAndInches(int inches)
+    {
+        int feet = inches / 12;
+        int remainingInches = inches % 12;
+        return $"{feet}'{remainingInches}\"";
+    }
+
+    public static double ConvertFeetAndInchesToCentimeters(string feetAndInches)
+    {
+        int inches = ConvertFeetAndInchesToInches(feetAndInches);
+        return ConvertInchesToCentimeters(inches);
+    }
+
+    public static double ConvertInchesToCentimeters(int inches)
+    {
+        return inches * 2.54;
+    }
+
+    public static string ConvertCentimetersToFeetAndInches(double centimeters)
+    {
+        int inches = Convert.ToInt32(centimeters / 2.54);
+        return ConvertInchesToFeetAndInches(inches);
+    }
+
+    public static double ConvertCentimetersToInches(double centimeters)
+    {
+        return centimeters / 2.54;
+    }
 }
--- DnD35EDMToolsTests/Test_ConversionHelpers.cs
+++ DnD35EDMToolsTests/Test_ConversionHelpers.cs
@@ -1,18 +1,59 @@
 ﻿using DnD35EDMTools.Helpers;
 
-namespace DnD35EDMToolsTests;
-
-public class ConversionHelpersTests
+namespace DnD35EDMToolsTests
 {
-    [Fact]
-    public void test_ConvertInchesToFeet()
+    public class ConversionHelpersTests
     {
-        // Arrange
-        
-        // Act
-        var result = ConversionHelper.ConvertInchesToFeet(73);
+        [Fact]
+        public void Test_ConvertFeetAndInchesToInches()
+        {
+            // Arrange
+            string feetAndInches = "6'1\"";
 
-        //Assert
-        Assert.Equal(@"6'1""", result);
+            // Act
+            var result = ConversionHelper.ConvertFeetAndInchesToInches(feetAndInches);
+
+            // Assert
+            Assert.Equal(73, result);
+        }
+
+        [Fact]
+        public void Test_ConvertInchesToFeetAndInches()
+        {
+            // Arrange
+            int inches = 73;
+
+            // Act
+            var result = ConversionHelper.ConvertInchesToFeetAndInches(inches);
+
+            // Assert
+            Assert.Equal("6'1\"", result);
+        }
+
+        [Fact]
+        public void Test_ConvertFeetAndInchesToCentimeters()
+        {
+            // Arrange
+            string feetAndInches = "6'1\"";
+
+            // Act
+            var result = ConversionHelper.ConvertFeetAndInchesToCentimeters(feetAndInches);
+
+            // Assert
+            Assert.Equal(185.42, result, 2); // Allowing for some precision error
+        }
+
+        [Fact]
+        public void Test_ConvertCentimetersToFeetAndInches()
+        {
+            // Arrange
+            double centimeters = 185.42;
+
+            // Act
+            var result = ConversionHelper.ConvertCentimetersToFeetAndInches(centimeters);
+
+            // Assert
+            Assert.Equal("6'1\"", result);
+        }
     }
 }
