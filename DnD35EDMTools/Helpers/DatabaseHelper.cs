4d35bdce6fbc137094fc41c1b85524675c8aef0b
MGNThalentyr <morkai.bde@gmail.com>
2024-02-26 00:42:28 +0200

    WIP PT1
    Added initial code for to get list of Deities list based on selected Order and Morality

---
+++ DnD35EDMTools/Helpers/DatabaseHelper.cs
@@ -0,0 +1,37 @@
+﻿using DnD35EDMTools.Data;
+using Microsoft.EntityFrameworkCore;
+namespace DnD35EDMTools.Helpers;
+
+public class DatabaseHelper
+{
+    private List<RaceData> _races = new List<RaceData>();
+    // private List<AlignmentData> _alignments = new List<AlignmentData>();
+    private static List<OrderData> _orders = new List<OrderData>();
+    private List<MoralityData> _moralities = new List<MoralityData>();
+    private List<ClassData> _classes = new List<ClassData>();
+    private List<GenderData> _genders = new List<GenderData>();
+    private List<DeityData> _deities = new List<DeityData>();
+
+    // public static void PopulateOrdersFromDB()
+    // {
+    //     using var  db = DbContext;
+    //     var ordersFromDb = db.Orders.ToList();
+    //     _orders.AddRange(ordersFromDb);
+    // }
+
+    public static string GetOrderNameFromDB(int orderValue)
+    {
+        // if (_orders.Count == 0)
+        // {
+        //         PopulateOrdersFromDB();
+        // }
+        var orderName = _orders.FirstOrDefault(o => orderValue >= o.MinValue && orderValue <= o.MaxValue);
+        return orderName.Order;
+    }
+    
+    public string GetMoralityNameFromDB(int moralityValue)
+    {
+        var moralityName = _orders.FirstOrDefault(o => moralityValue >= o.MinValue && moralityValue <= o.MaxValue);
+        return moralityName.Order;
+    }
+}
---
+++ DnD35EDMToolsTests/Test_DatabaseHelpers.cs
@@ -0,0 +1,19 @@
+﻿using DnD35EDMTools.Helpers;
+
+namespace DnD35EDMToolsTests;
+
+public class Test_DatabaseHelpers
+{
+    [Fact]
+    public void Test_GetOrderNameFromDB()
+    {
+        // Arrange
+        int orderValue = 20;
+
+        // Act
+        var result = DatabaseHelper.GetOrderNameFromDB(orderValue);
+
+        // Assert
+        Assert.Equal("Chaotic", result);
+    }
+}
