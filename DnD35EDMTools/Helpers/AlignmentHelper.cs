32232ab893aacc9857809fb676894d47bef6a262
MGNThalentyr <morkai.bde@gmail.com>
2024-02-28 00:07:53 +0200

    Update Deities
    Updated CharacterCreation to get Alignment based on Order and Morality
    Removed DatabaseHelpers and tests
    Added AlignmentHelper and Tests
    Added In Memory testing to project

---
+++ DnD35EDMTools/Helpers/AlignmentHelper.cs
@@ -0,0 +1,29 @@
+﻿using DnD35EDMTools.Data;
+namespace DnD35EDMTools.Helpers;
+
+public static class AlignmentHelper
+{
+
+    public static string GetOrderNameByValue(List<OrderData> orders, int orderValue)
+    {
+        var order = orders.FirstOrDefault(o => orderValue >= o.MinValue && orderValue <= o.MaxValue);
+        return order.Order;
+    }
+    
+    public static string GetMoralityNameByValue(List<MoralityData> moralities, int moralityValue)
+    {
+        var morality = moralities.FirstOrDefault(o => moralityValue >= o.MinValue && moralityValue <= o.MaxValue);
+        return morality.Morality;
+    }
+
+    public static string GetAlignmentFromOrderAndMorality(List<OrderData> orders, List<MoralityData> moralities,
+        int orderValue, int moralityValue)
+    {
+        var order = orders.FirstOrDefault(o => orderValue >= o.MinValue && orderValue <= o.MaxValue);
+        var morality = moralities.FirstOrDefault(o => moralityValue >= o.MinValue && moralityValue <= o.MaxValue);
+        var alignment = $"{order.Order} {morality.Morality}";
+        if (alignment == "Neutral Neutral")
+            alignment = "True Neutral";
+        return alignment;
+    }
+}
