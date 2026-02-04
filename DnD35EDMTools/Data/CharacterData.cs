dc0ffd6266719b9fc767d63ef6129b6b8b0d9acc
MGNThalentyr <morkai.bde@gmail.com>
2026-02-05 01:36:18 +0200

    3.5E-13-Fix-Unhandled-Exception-During-Character-Creation (#18)
    * Update Character Creation
    
    Moved code from PhysicalAttributes to CharacterCreation.
    Removed PhysicalAttributes.
    
    * Update Character Creation
    
    Housekeeping

--- DnD35EDMTools/Data/CharacterData.cs
+++ DnD35EDMTools/Data/CharacterData.cs
@@ -28,8 +28,8 @@ public class CharacterData
     public int Strength { get; set; }
     public int Dexterity { get; set; }
     public int Constitution { get; set; }
-    public int Wisdom { get; set; }
     public int Intelligence { get; set; }
+    public int Wisdom { get; set; }
     public int Charisma { get; set; }
     public List<int> Languages { get; set; } = [1];
     public decimal BaseFortitudeSave { get; set; }
