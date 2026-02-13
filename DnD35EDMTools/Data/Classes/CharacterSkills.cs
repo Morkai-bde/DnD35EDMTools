95e2c9282311dda218e4dce86c050d8e821de206
MGNThalentyr <morkai.bde@gmail.com>
2026-02-13 21:06:04 +0200

    Update Character Creation
    Reworked temp UI to fit all skills currently available.
    Implemented loading of the skills list and some boiler plate for the next steps.

---
+++ DnD35EDMTools/Data/Classes/CharacterSkills.cs
@@ -0,0 +1,9 @@
+﻿namespace DnD35EDMTools.Data.Classes;
+
+public class CharacterSkills
+{
+ public int Id { get; set; }
+ public int SkillDefinitionId { get; set; }
+ public string? Specialization { get; set; }
+ public int Ranks { get; set; }
+}
