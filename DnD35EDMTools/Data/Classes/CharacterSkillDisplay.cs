6ce4e61acacd735d5dd0f431bee9f8e1673bf98e
MGNThalentyr <morkai.bde@gmail.com>
2026-02-14 01:16:52 +0200

    Update Character Template
    Added skills to Character Template
    Fixed typo in style.css

---
+++ DnD35EDMTools/Data/Classes/CharacterSkillDisplay.cs
@@ -0,0 +1,9 @@
+﻿namespace DnD35EDMTools.Data.Classes;
+
+public class CharacterSkillDisplay
+{
+    public string Name { get; set; } = "";
+    public int Ranks { get; set; }
+    public int Modifier { get; set; }
+    public bool IsTrainedOnly { get; set; }
+}
