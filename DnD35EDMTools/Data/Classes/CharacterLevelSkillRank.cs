9c9d167f963fe99ca168ee23fc22599738e180f8
MGNThalentyr <morkai.bde@gmail.com>
2026-02-19 23:38:26 +0200

    Update DB
    Added Class skill field.

--- DnD35EDMTools/Data/Classes/CharacterLevelSkillRank.cs
+++ DnD35EDMTools/Data/Classes/CharacterLevelSkillRank.cs
@@ -6,6 +6,7 @@ public class CharacterLevelSkillRank
     public int CharacterLevelId { get; set; }
     public int SkillId { get; set; }
     public int RanksPurchased { get; set; }
+    public bool IsClassSkill { get; set; }
     public CharacterLevel CharacterLevel { get; set; }
     public SkillData Skill { get; set; }
 }
---
+++ DnD35EDMTools/Data/Migrations/20260219212630_AddClassSkillsToSkillRanks.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DnD35EDMTools.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClassSkillsToSkillRanks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsClassSkill",
+                table: "JoinTableCharacterLevelSkillRanks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsClassSkill",
+                table: "JoinTableCharacterLevelSkillRanks");
+        }
+    }
+}
