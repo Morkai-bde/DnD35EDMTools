f23b5b9d4b5f2b4f8641d4dfc7bcf3a8388d4202
MGNThalentyr <morkai.bde@gmail.com>
2026-02-27 23:15:13 +0200

    Update Character Template
    Add Size Helper
    Add Equipped armour and shield calculations to AC

---
+++ DnD35EDMTools/Helpers/SizeHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace DnD35EDMTools.Helpers;
+
+public static class SizeHelper
+{
+    public static int GetSizeAcBonus(string size)
+    {
+        switch (size)
+        {
+            case "Fine" : return 8;
+            case "Diminutive" : return 4;
+            case "Tiny" : return 2;
+            case "Small" : return 1;
+            case "Medium" : return 0;
+            case "Large" : return -1;
+            case "Huge" : return -2;
+            case "Gargantuan" : return -4;
+            case "Colossal" : return -8;
+            default : return 0;
+        }
+    }
+}
