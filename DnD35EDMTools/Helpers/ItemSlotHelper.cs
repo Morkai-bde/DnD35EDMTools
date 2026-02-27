bc1433051e7f049db6858f2e7600917a29489935
MGNThalentyr <morkai.bde@gmail.com>
2026-02-27 11:59:53 +0200

    Add Character Management
    Added a page to equip equipment.

---
+++ DnD35EDMTools/Helpers/ItemSlotHelper.cs
@@ -0,0 +1,25 @@
+﻿using DnD35EDMTools.Data.Enums;
+
+namespace DnD35EDMTools.Helpers;
+
+public static class ItemSlotHelper
+{
+    public static string GetSlotName(ItemSlot slot) => slot switch
+    {
+        ItemSlot.None => "None",
+        ItemSlot.Head => "Head",
+        ItemSlot.Face => "Face",
+        ItemSlot.Neck => "Neck",
+        ItemSlot.Shoulders => "Shoulders",
+        ItemSlot.Body => "Body",
+        ItemSlot.Torso => "Torso",
+        ItemSlot.Waist => "Waist",
+        ItemSlot.Wrists => "Wrists",
+        ItemSlot.Hands => "Hands",
+        ItemSlot.Ring => "Ring",
+        ItemSlot.Feet => "Feet",
+        ItemSlot.Weapon => "Weapon",
+        ItemSlot.Shield => "Shield",
+        _ => slot.ToString()
+    };
+}
